fdd5ef7092b53b64b8396a5b6dd16fe6d3dfe7e2
Zwylair <zwylair@gmail.com>
2024-04-16 22:09:21 +0300

    Initial commit

---
+++ app/src/main/java/com/zwylair/farmmon/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.zwylair.farmmon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
+//import android.widget.Toast
+import com.zwylair.farmmon.databinding.ActivityMainBinding
+import com.fazecast.jSerialComm.SerialPort
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val updateButton = findViewById<Button>(R.id.update_button)
+        val moistureValue = findViewById<TextView>(R.id.moistureValue1)
+
+        updateButton.setOnClickListener {
+//            Toast.makeText(applicationContext, "Кнопка нажата!", Toast.LENGTH_SHORT).show()
+            moistureValue.text = "sdflsdf"
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.  ## ActionBar
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
